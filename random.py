00f4405493d0e7a78a58b38463fb55a2947e0afb
Tangaraj, Sodexis <tangaraj@sodexis.com>
2015-09-10 11:19:50 +0530

    New file added random.py

---
+++ random.py
@@ -0,0 +1,16 @@
+{
+"name":"super market",
+"description":"""
+product for sales""",
+"category":"sales",
+"depends":["base",'website'],
+"data":[
+        "views/menu.xml",
+        "views/salesreq.xml",
+        'views/js.xml',
+        ],
+        'js': ['/static/src/js/webexample.js'],
+        'css': ['/static/src/css/in.css'],
+        'qweb': ['/static/src/xml/jsqweb.xml'],
+
+}
