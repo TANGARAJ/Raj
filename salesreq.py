528b81dadef289dfcca664b8d5172c2f9200c086
Tangaraj, Sodexis <tangaraj@sodexis.com>
2015-09-10 11:30:09 +0530

    Message from 100:salesreq.py

---
+++ salesreq.py
@@ -0,0 +1,41 @@
+from openerp import models,fields,api,_ 
+class salesreq(models.Model):
+    _name="supermarket"
+    
+    customer=fields.Char()
+    product_name=fields.Char(string="product details")
+
+    @api.multi
+    def my_method(self):
+        res_ids=self.search([('id','=',self.id)]).ids
+        print self.ids, 'tangaraj'
+
+
+# @api.one 
+# def salesvv_reqss(self):
+#     
+#     global i
+#  
+#     if(salesreq.i<len(self.list1)):
+#         self.product_name=self.list1[salesreq.i]
+#         salesreq.i=salesreq.i+1
+#         print salesreq.i
+#     else:
+#         self.product_name="Selection is over!"
+#         salesreq.i=0
+        
+        
+        
+#     
+#     list1=["LUX SOAP",'life boy',
+#            "liril","nature power",
+#            "cinthol",
+#            "hamam",
+#            "pears"]
+#     
+#     
+#     i=0
+# 
+
+        
+                          
